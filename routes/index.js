c22d66881ad41e1a4ab710a99bf8324182820d60
Max <asddsajpg@gmail.com>
2017-06-03 14:28:09 +0400

    Connected ro database

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/jsonexample', function (req, res, next) {
+    res.json({name1: 'lalala', name2: 'nanana'});
 });
 
 module.exports = router;
